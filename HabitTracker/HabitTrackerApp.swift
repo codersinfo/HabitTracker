8490c39a0585c91ddade1bcfd453c354bba5b747
codersinfo <coders.infoapp@gmail.com>
2023-09-28 19:51:07 +0530

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Priya Shankar on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
